a71a2d89d0c781668a09066e7101039da1e4c6de
Melisa Andreea Bogdan <melisa.bogdan@gmail.com>
2024-12-07 11:30:41 +0000

    day 7 part 2 in progress

--- src/main/java/com/example/RopeBridge.java
+++ src/main/java/com/example/RopeBridge.java
@@ -22,12 +22,64 @@ public class RopeBridge {
         return totalSum;
     }
 
-    public int result_part2() {
+    public long result_part2() {
+        // List<long[]> parsedData = readFileAndParse();
+        // long totalSum = 0;
+        // for (long[] array : parsedData) {
+        //     long target = array[0];
+        //     StringBuilder expression = new StringBuilder();
+        //     if (isValidCombinationWithConcatenation(array, target, expression)) {
+        //         System.err.println("Valid array: " + arrayToString(array) + " with expression: " + expression.toString());
+        //         totalSum += target;
+        //     }
+        // }
+        // System.err.println("Total sum of valid arrays: " + totalSum);
+        // return totalSum;
         return 0;
     }
 
     private static boolean isValidCombination(long[] array, long target, StringBuilder expression) {
         return isValidCombinationHelper(array, target, 1, array[1], new StringBuilder(Long.toString(array[1])), expression);
+    }
+
+    private static boolean isValidCombinationWithConcatenation(long[] array, long target, StringBuilder expression) {
+        return isValidCombinationHelperWithConcatenation(array, target, 1, array[1], new StringBuilder(Long.toString(array[1])), expression);
+    }
+
+    private static boolean isValidCombinationHelperWithConcatenation(long[] array, long target, int index, long currentValue, StringBuilder currentExpression, StringBuilder finalExpression) {
+        if (index == array.length - 1) {
+            if (currentValue == target) {
+                finalExpression.append(currentExpression);
+                return true;
+            }
+            return false;
+        }
+
+        long nextValue = array[index + 1];
+
+        // Try addition
+        currentExpression.append(" + ").append(nextValue);
+        if (isValidCombinationHelperWithConcatenation(array, target, index + 1, currentValue + nextValue, currentExpression, finalExpression)) {
+            return true;
+        }
+        currentExpression.setLength(currentExpression.length() - (" + " + nextValue).length());
+
+        // Try multiplication
+        currentExpression.append(" * ").append(nextValue);
+        if (isValidCombinationHelperWithConcatenation(array, target, index + 1, currentValue * nextValue, currentExpression, finalExpression)) {
+            return true;
+        }
+        currentExpression.setLength(currentExpression.length() - (" * " + nextValue).length());
+
+        // Try concatenation
+        currentExpression.append(" || ").append(nextValue);
+        long concatenatedValue = Long.parseLong(currentValue + "" + nextValue);
+        if (isValidCombinationHelperWithConcatenation(array, target, index + 1, concatenatedValue, currentExpression, finalExpression)) {
+            return true;
+        }
+        currentExpression.setLength(currentExpression.length() - (" || " + nextValue).length());
+
+        return false;
     }
 
     private static boolean isValidCombinationHelper(long[] array, long target, int index, long currentValue, StringBuilder currentExpression, StringBuilder finalExpression) {
